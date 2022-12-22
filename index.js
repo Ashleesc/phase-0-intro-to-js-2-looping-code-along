250a309a769bd916c0334390f7cdb8d84fd31694
Ashlee Scott Gregory <ashlee.scott@gmail.com>
2022-12-22 10:57:54 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Code your solutions in this file
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+// let names = ["Ashlee", "Todd", "LaRene", "Dana"]
+
+// function writeCards(names, party) {
+//     for (let i= 0; i < names.length; i++) {
+//         return (`Happy Holidays, ${names[i]}, thanks for coming to my ${party}`)
+//     }
+//     return writeCards(names, party)
+// }
+
+function writeCards(names,party) {
+    for (let i=0; i < names.length; i++){
+        writeCards.push(`Thank you, ${names[i]} for coming to my holiday ${party}`)
+    }
+    return writeCards
+}
+   
+let countup = 0
+while (countup < 0) {
+    console.log(countup++)
+}
+    
+for (let countup = 0; countup < 10; countup++) {
+    console.log(countup);
+}
+// these two are the same and achieve the same thing
+
+function countDown (start) {
+while (start > 0) {
+    console.log(start--)
+}
+console.log(start)
+}
+
